13f6033bd9f2bd032b157fdad8eee475c51705ae
ochim <ochimasumi@gmail.com>
2019-02-16 23:07:20 +0900

    GCD(最大公約数)の追加

--- テンプレ開発/main.py
+++ テンプレ開発/main.py
@@ -170,6 +170,12 @@ class Idea:
         """
         return sum(a) / len(a)
 
+    def gcd(self, x, y):
+        if y == 0:
+            return x
+        else:
+            return self.gcd(y, x % y)
+
 
 """ここからメインコード"""
 
